4fbc64018b21f0b8b37c72b13c06076e9f24ece6
Yulia <juliam2007@mail.ru>
2018-01-25 20:32:01 +0300

    Code refactoring

--- js/pictures.js
+++ js/pictures.js
@@ -1,6 +1,9 @@
 'use strict';
 
 var COUNT_PHOTOS = 25;
+var MIN_LIKES = 15;
+var MAX_LIKES = 200;
+
 var COMMENTS = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
 
 var picturesList = document.querySelector('.pictures'); // Найдем элемент в который мы будем вставлять наши изображения
@@ -53,7 +56,7 @@ function generateNotes() {
   for (var i = 1; i < COUNT_PHOTOS + 1; i++) {
     notes.push({
       url: 'photos/' + i + '.jpg',
-      likes: getRandomNumber(15, 200),
+      likes: getRandomNumber(MIN_LIKES, MAX_LIKES),
       comments: getRandomElement(COMMENTS)
     });
   }
