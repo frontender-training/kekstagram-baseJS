b6e2de5e733a3b7061bcd739ed2ebb1dd6cfaad9
Yulia <juliam2007@mail.ru>
2018-01-31 15:54:59 +0300

    Code refactoring

--- js/pictures.js
+++ js/pictures.js
@@ -1,30 +1,30 @@
 'use strict';
 
-var COUNT_PHOTOS = 25;
-var MIN_LIKES = 15;
-var MAX_LIKES = 200;
+var DataPicture = {
+  COUNT_PHOTOS: 25,
+  MIN_LIKES: 15,
+  MAX_LIKES: 200,
+  COMMENTS: ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!']
+};
 
-var COMMENTS = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
-
-var picturesList = document.querySelector('.pictures'); // Найдем элемент в который мы будем вставлять наши изображения
+var picturesList = document.querySelector('.pictures');                     // Найдем элемент в который мы будем вставлять наши изображения
 var picturesTemplate = document.querySelector('#picture-template').content; // Найдем шаблон который мы будем копировать.
+var uploadPicture = document.querySelector('.upload-overlay');              // Найдем окно загрузки фотографий.
+var gallery = document.querySelector('.gallery-overlay');                   // Найдем окно для просмотра фотографий
 
 var listNotes = generateNotes();
 
-clonePicture();
+renderPicturesList();
 closeUploadPopup();
 openPicture();
 
 // Закрываем окно загрузки фотографий
 function closeUploadPopup() {
-  var uploadPicture = document.querySelector('.upload-overlay');
   uploadPicture.classList.add('invisible');
 }
 
 // Открываем первую фотографию
 function openPicture() {
-  var gallery = document.querySelector('.gallery-overlay');
-
   gallery.classList.remove('invisible');
   gallery.querySelector('.gallery-overlay-image').src = listNotes[0].url;
   gallery.querySelector('.likes-count').textContent = listNotes[0].likes;
@@ -32,7 +32,7 @@ function openPicture() {
 }
 
 // Клонируем фотографии
-function clonePicture() {
+function renderPicturesList() {
   var fragment = document.createDocumentFragment();
 
   for (var i = 0; i < listNotes.length; i++) {
@@ -53,11 +53,11 @@ function renderPicture(image) {
 // Функция, возвращающаая массив объектов записей в блоге
 function generateNotes() {
   var notes = [];
-  for (var i = 1; i < COUNT_PHOTOS + 1; i++) {
+  for (var i = 1; i < DataPicture.COUNT_PHOTOS + 1; i++) {
     notes.push({
       url: 'photos/' + i + '.jpg',
-      likes: getRandomNumber(MIN_LIKES, MAX_LIKES),
-      comments: getRandomElement(COMMENTS)
+      likes: getRandomNumber(DataPicture.MIN_LIKES, DataPicture.MAX_LIKES),
+      comments: getRandomElement(DataPicture.COMMENTS)
     });
   }
   return notes;
