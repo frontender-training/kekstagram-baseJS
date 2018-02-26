eda1c7bc03eac0b933f843a17f56750e9daf9496
cybersunt <juliam2007@mail.ru>
2018-02-26 13:57:34 +0300

    Add events for upload form

--- js/pictures.js
+++ js/pictures.js
@@ -1,26 +1,59 @@
 'use strict';
 
-var DataPicture = {
+var DATA_PICTURE = {
   COUNT_PHOTOS: 25,
   MIN_LIKES: 15,
   MAX_LIKES: 200,
   COMMENTS: ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!']
 };
 
+var KEYCODE = {
+  ENTER: 13,
+  ESC: 27
+};
+
 var picturesList = document.querySelector('.pictures');                     // Найдем элемент в который мы будем вставлять наши изображения
 var picturesTemplate = document.querySelector('#picture-template').content; // Найдем шаблон который мы будем копировать.
 var uploadOverlay = document.querySelector('.upload-overlay');              // Найдем окно загрузки фотографий.
+var uploadForm = document.querySelector('.upload-form');
+
+var uploadFormFile = uploadForm.querySelector('#upload-file');
+
+var uploadFormCancel = document.querySelector('.upload-form-cancel');
+
+var uploadFormComment = uploadForm.querySelector('.upload-form-description');
+
 var galleryOverlay = document.querySelector('.gallery-overlay');                   // Найдем окно для просмотра фотографий
 
 var galleryOverlayClose = galleryOverlay.querySelector('.gallery-overlay-close');
 
 var listNotes = generateNotes();
 
+uploadFormFile.addEventListener('change', openUploadOverlay);
+
+uploadFormCancel.addEventListener('click', closeUploadOverlay);
+
 galleryOverlayClose.addEventListener('click', closePicture);
+
+// Функция закрытия окна редактирования фото по клику на ESC
+function onUploadOverlayEscPress(evt) {
+  if (evt.keyCode === KEYCODE.ESC) {
+    closeUploadOverlay()
+  }
+}
 
 // Закрываем окно загрузки фотографий
 function closeUploadOverlay() {
-  uploadOverlay.classList.add('invisible');
+  if (document.activeElement !== uploadFormComment) {
+    uploadOverlay.classList.add('invisible');
+    document.removeEventListener('keydown', onUploadOverlayEscPress);
+  }
+}
+
+// Открываем окно загрузки фотографий
+function openUploadOverlay() {
+  uploadOverlay.classList.remove('invisible');
+  document.addEventListener('keydown', onUploadOverlayEscPress);
 }
 
 // Открываем фотографию
@@ -46,6 +79,8 @@ function renderPicturesList(array, container) {
   });
 
   container.appendChild(fragment);
+
+  uploadForm.classList.remove('invisible');
 }
 
 function generatePicturePreview(image, index) {
@@ -73,11 +108,11 @@ function renderPictures(image) {
 // Функция, возвращающаая массив объектов записей в блоге
 function generateNotes() {
   var notes = [];
-  for (var i = 1; i < DataPicture.COUNT_PHOTOS + 1; i++) {
+  for (var i = 1; i < DATA_PICTURE.COUNT_PHOTOS + 1; i++) {
     notes.push({
       url: 'photos/' + i + '.jpg',
-      likes: getRandomNumber(DataPicture.MIN_LIKES, DataPicture.MAX_LIKES),
-      comments: getRandomElement(DataPicture.COMMENTS)
+      likes: getRandomNumber(DATA_PICTURE.MIN_LIKES, DATA_PICTURE.MAX_LIKES),
+      comments: getRandomElement(DATA_PICTURE.COMMENTS)
     });
   }
   return notes;
